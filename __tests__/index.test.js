970d0d44f12c0e0a62b96ce41d58b81d4094c140
Andrey Barmenkov <barmenkov.av@yandex.ru>
2022-02-11 10:55:44 +0300

    tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,26 +9,19 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const getFixedPath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-// const readFile = (filename) => fs.readFileSync(getFixedPath(filename), 'utf-8');
-// const json = readFile('result-json.txt');
+const readFile = (filename) => fs.readFileSync(getFixedPath(filename), 'utf-8');
 
-const expected = `{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}`;
+const json = readFile('result-json.txt');
+const stylish = readFile('result-stylish.txt');
 
 test('test-json', () => {
   const filepath1 = getFixedPath('file1.json');
   const filepath2 = getFixedPath('file2.json');
-  expect(genDiff(filepath1, filepath2)).toBe(expected);
+  expect(genDiff(filepath1, filepath2)).toBe(json);
 });
 
 test('test-yml', () => {
   const filepath1 = getFixedPath('file1.yml');
   const filepath2 = getFixedPath('file2.yml');
-  expect(genDiff(filepath1, filepath2)).toBe(expected);
+  expect(genDiff(filepath1, filepath2)).toBe(stylish);
 });
